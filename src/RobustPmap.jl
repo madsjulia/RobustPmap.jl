2ae7be7740600ad4db9cb6ec87a0b0fcfee9f3cd
monty <velimir.vesselinov@gmail.com>
2018-07-09 19:13:43 -0600

    extra line deleted

--- src/RobustPmap.jl
+++ src/RobustPmap.jl
@@ -29,7 +29,6 @@ PROCESS DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT INFRINGE PRIVATELY OWNED
 
 LA-CC-15-080; Copyright Number Assigned: C16008
 """
-
 module RobustPmap
 
 import Compat.String
